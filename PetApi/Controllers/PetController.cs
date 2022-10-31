5e13a26a64bbcf30f4f4a7729862fa1d510325d6
hsun15 <hsun15@slb.com>
2022-10-31 17:37:38 +0800

    AC1 and AC2

---
+++ PetApi/Controllers/PetController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetApi.Model;
+using System.Collections.Generic;
+
+namespace PetApi.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("api")]
+    public class PetController : ControllerBase
+    {
+        private static List<Pet> pets = new List<Pet>();
+            
+        [HttpPost("addNewPet")]
+        public Pet AddNewPet(Pet pet)
+        {
+            pets.Add(pet);
+            return pet;
+        }
+
+        [HttpGet("getAllPets")]
+        public List<Pet> GetAllPets()
+        {
+            return pets;
+        }
+
+        [HttpDelete("deleteAllPets")]
+        public List<Pet> DeleteAllPets(List<Pet> pets)
+        {
+            pets.Clear();
+            return pets;
+        }
+    }
+}
